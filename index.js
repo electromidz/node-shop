50e81bdedbec533678564c736cea584965c89e62
electromidz <electromidz@gmail.com>
2023-05-28 20:54:42 +0330

    Add structure and need add mongoos Docker clustering

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(port, () => {
+  console.log("App listening on port " + port);
+});
