77c60306d76a54230f804b64b62aca050f780581
electromidz <electromidz@gmail.com>
2023-06-01 14:33:10 +0330

    Add user Schema

--- models/product.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const productSchema = new mongoose.Schema({
-  name: String,
-});
-
-const ProductModel = mongoose.model("Product", productSchema);
-
-module.exports = ProductModel;
