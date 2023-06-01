af173162b35f5f5743a7f6b4014eea6336c0d5eb
electromidz <electromidz@gmail.com>
2023-06-01 14:37:38 +0330

    Complete user model

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
-mongoose.connection.on("error", (error) => {
-  console.error(`Error connecting to DATABASE: ${error}`);
-});
+// mongoose.connection.on("error", (error) => {
+//   console.error(`Error connecting to DATABASE: ${error}`);
+// });
 
-mongoose.connection.on("open", () => {
-  console.log(
-    `Establishing connected to DATABASE [${process.env.DATABASE_NAME}] -> 💾`
-  );
-});
+// mongoose.connection.on("open", () => {
+//   console.log(
+//     `Establishing connected to DATABASE [${process.env.DATABASE_NAME}] -> 💾`
+//   );
+// });
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`App listening on port [${process.env.SERVER_PORT}] ... 🐳`);
