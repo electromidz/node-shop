05a7dd768b9947920f7e03bf574c5703fc4c0dc2
electromidz <electromidz@gmail.com>
2023-06-01 14:14:52 +0330

    start working

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 8080;
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/db-shop", {
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 mongoose.connection.on("error", (error) => {
-  console.error(`Error connecting: ${error}`);
+  console.error(`Error connecting to DATABASE: ${error}`);
 });
 
 mongoose.connection.on("open", () => {
-  console.log("Establishing connected ...");
+  console.log(
+    `Establishing connected to DATABASE [${process.env.DATABASE_NAME}] -> 💾`
+  );
 });
 
-const ProductModel = require("./models/product");
-const newProduct = new ProductModel({ name: "new product" });
-async function saveProduct() {
-  await newProduct.save();
-}
-saveProduct();
-console.log("Product saved", newProduct);
-
-app.listen(port, () => {
-  console.log("App listening on port " + port);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`App listening on port [${process.env.SERVER_PORT}] ... 🐳`);
 });
 
 // 178.22.122.100, 185.51.200.2
