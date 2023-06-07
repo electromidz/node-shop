59f3ff32c2728c96ce7517e3ae076343b1cf5857
electromidz <electromidz@gmail.com>
2023-06-07 23:15:09 +0330

    Work on validation regitry form

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 function auth(req, res, next) {
   console.log(req.headers);
   const token = req.headers["authorization"];
-  if (!token) {
-    return res.status(401).send({ message: "Not authenticated!" });
-  } else {
-    jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
-      if (err) res.status(401).send({ message: err.message });
-      req.user = decoded;
-      next();
-    });
-  }
+  // if (!token) {
+  //   return res.status(401).send({ message: "Not authenticated!" });
+  // } else {
+  // jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+  //   if (err) res.status(401).send({ message: err.message });
+  //   req.user = decoded;
+  next();
+  // });
+  // }
 }
 
 module.exports = auth;
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const Joi = require("joi");
 
 const userModel = require("../models/user");
 
@@ -13,6 +14,23 @@ async function hashPass(password) {
 }
 
 router.post("/register", async (req, res) => {
+  const userSchema = Joi.object({
+    name: Joi.string().min(3).max(100).required(),
+    email: Joi.string()
+      .min(3)
+      .max(320)
+      .required()
+      .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
+    phone: Joi.string()
+      .min(11)
+      .max(11)
+      .required()
+      .pattern(/09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/),
+    password: Joi.string().required().min(6).max(20),
+  });
+  const userValidation = userSchema.validate(req.body);
+  console.log("Validation::", userValidation);
+  return 0;
   req.body.password = await hashPass(req.body.password);
   const newUser = new userModel(req.body);
   if (newUser) {
