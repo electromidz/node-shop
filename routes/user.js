e8025ea4e704e77616bc0701562323d15213f6d4
electromidz <electromidz@gmail.com>
2023-06-16 23:30:24 +0330

    I need to add more time on it

--- routes/user.js
+++ routes/user.js
@@ -5,22 +5,22 @@ const jwt = require("jsonwebtoken");
 
 const { UserModel, userValidation, hashPassword } = require("../models/user");
 
-// router.post("/register", async (req, res) => {
-//   const validation = userValidation(req.body);
-//   console.log("ZOD ERROR ::\n", validation);
-//   if (validation.error) {
-//     return res.status(404).send(validation.error.details[0].message);
-//   }
-//   req.body.password = await hashPassword(req.body.password);
-//   console.log(req.body);
-//   const newUser = new UserModel(req.body);
-//   try {
-//     await newUser.save();
-//     res.status(201).send(newUser);
-//   } catch (err) {
-//     res.status(500).send({ error: "Somtthings went wrong" });
-//   }
-// });
+router.post("/register", async (req, res) => {
+  const validation = userValidation(req.body);
+  console.log("ZOD ERROR ::\n", validation);
+  if (validation.error) {
+    return res.status(404).send(validation.error.details[0].message);
+  }
+  req.body.password = await hashPassword(req.body.password);
+  console.log(req.body);
+  const newUser = new UserModel(req.body);
+  try {
+    await newUser.save();
+    res.status(201).send(newUser);
+  } catch (err) {
+    res.status(500).send({ error: "Somtthings went wrong" });
+  }
+});
 
 router.post("/login", async (req, res) => {
   const { email, phone, password } = req.body;
