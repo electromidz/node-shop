f78ac3135ce4063d423647d862be17b383f1c06e
electromidz <electromidz@gmail.com>
2023-06-26 23:26:12 +0330

    Add controllers to project

---
+++ controllers/user.js
@@ -0,0 +1,47 @@
+const { UserModel, userValidation, hashPassword } = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+module.exports = {
+  register: async function (req, res) {
+    const validation = userValidation(req.body);
+    if (validation.error) {
+      return res.status(404).send(validation.error.details[0].message);
+    }
+    req.body.password = await hashPassword(req.body.password);
+    console.log(req.body);
+    const newUser = new UserModel(req.body);
+    try {
+      await newUser.save();
+      res.status(201).send(newUser);
+    } catch (err) {
+      // TODO: must be handle error
+      res.status(500).send({ error: "Somtthings went wrong" });
+    }
+  },
+  login: async function (req, res) {
+    const { email, phone, password } = req.body;
+    const foundUser = await userModel.findOne({
+      $or: [{ email }, { phone }, { password }],
+    });
+
+    const user = await bcrypt.compareSync(password, foundUser.password); // true
+
+    if (user && foundUser) {
+      const token = jwt.sign(
+        { _id: foundUser._id, name: foundUser.name },
+        process.env.JWT_PRIVATE_KEY
+      );
+      const decode = jwt.decode(token);
+      console.log(decode);
+
+      res.status(200);
+      return res.send(token);
+    } else if (!user) {
+      res.status(401);
+      res.send({ error: "Wrong password" });
+    } else {
+      res.status(401);
+      res.send({ error: "Wrong password" });
+    }
+  },
+};
