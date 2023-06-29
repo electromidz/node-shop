0dbc37e0169bdb4522b673ba0a8d07491abb5ef7
electromidz <electromidz@gmail.com>
2023-06-29 16:48:35 +0330

    Work on User modal

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 function auth(req, res, next) {
-  console.log(req.headers);
-  const token = req.headers["authorization"];
-  if (!token) {
-    return res.status(401).send({ message: "Not authenticated!" });
-  } else {
-    jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
-      if (err) res.status(401).send({ message: err.message });
-      req.user = decoded;
-      next();
-    });
-  }
+  // console.log(req.headers);
+  // const token = req.headers["authorization"];
+  // if (!token) {
+  //   return res.status(401).send({ message: "Not authenticated!" });
+  // } else {
+  //   jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+  //     if (err) res.status(401).send({ message: err.message });
+  //     req.user = decoded;
+  next();
+  //   });
+  // }
 }
 
 module.exports = auth;
