00d5a79f4ecda88b61a185b6e4333c0502d27d20
electromidz <electromidz@gmail.com>
2023-07-10 23:13:34 +0330

    Add darticle schmema

--- models/article.js
+++ models/article.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose");
+
+const articleSchema = new mongoose,Schema({
+    title: {
+        type:String
+        required: true
+    },
+    content :{
+        type:String
+    }
+})
+
+const ArticleSchema = mongoose.model("Article", articleSchema);
+ module.exports = {ArticleSchema}
