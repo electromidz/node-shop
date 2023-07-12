10a1a798e062e828b1de97548184943853a0c3e1
electromidz <electromidz@gmail.com>
2023-07-12 22:56:03 +0330

    Work on ArticleModel

--- models/article.js
+++ models/article.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
-import {UserSchema} from './user';
-const {Schema} = mongoose;
+import { UserSchema } from "./user";
+const { Schema } = mongoose;
 
-const ArticleModel= mongoose.model("article", new Schema({
-    title: {
-        type:String
-        required: true,
-        minLength:3,
-        maxLength:300,
-    },
-    content :{
-        type:String
-    }
-     owner: userSchema
-})
+// const ArticleModel= mongoose.model("article", new Schema({
+//     title: {
+//         type:String
+//         required: true,
+//         minLength:3,
+//         maxLength:300,
+//     },
+//     content :{
+//         type:String
+//     }
+//      owner: userSchema
+// })
 
-;
- module.exports = {ArticleSchema}
+module.exports = { ArticleSchema };
