dad3e8c2e7b3a8c74406057dc122948b00d6f774
electromidz <electromidz@gmail.com>
2023-07-16 18:36:49 +0330

    Correct Prettier

--- models/article.js
+++ models/article.js
@@ -1,24 +1,27 @@
 const mongoose = require("mongoose");
-import { UserModel} from "./user";
+import { UserModel } from "./user";
 const { Schema } = mongoose;
 
-const ArticleModel= mongoose.model("article", new Schema({
+const ArticleModel = mongoose.model(
+  "article",
+  new Schema({
     title: {
-        type:String
-        required: true,
-        minLength:3,
-        maxLength:300,
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 300,
     },
-    content :{
-        type:String,
-required: true,
+    content: {
+      type: String,
+      required: true,
     },
-     owner:{type:Schema.Types.ObjectId, ref:'user'} ,
-     date:{
-        type:Date,
-        default:Date.now(),
-        required:true
-     }
-})
+    owner: { type: Schema.Types.ObjectId, ref: "user" },
+    date: {
+      type: Date,
+      default: Date.now(),
+      required: true,
+    },
+  })
+);
 
-module.exports = { ArticleModel};
+module.exports = { ArticleModel };
