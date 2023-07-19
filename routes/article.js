8ca524309febcacae1f2c174c806223a9d46ea7d
electromidz <electromidz@gmail.com>
2023-07-19 23:22:00 +0330

    Router still dont work

--- routes/article.js
+++ routes/article.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/article");
 
-// router.post("/article", articleController.postArticle);
-router.get("/article", (req, res) => {
-  console.log("here");
-});
+router.post("/article", articleController.postArticle);
+router.get("/article", articleController.geArticle);
 
 module.exports = router;
