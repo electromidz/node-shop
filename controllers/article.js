33f9217064ac43dbf50e49d0a790e669fa00c3ba
electromidz <electromidz@gmail.com>
2023-07-22 22:55:56 +0330

    Need validation on article

--- controllers/article.js
+++ controllers/article.js
@@ -2,8 +2,8 @@ const { ArticleModel, articleValidation } = require("../models/article");
 module.exports = {
   postArticle: async function (req, res) {
     console.log("POST Article", req.body);
-    const validation = articleValidation(req.body);
-    const articleSchema = ArticleModel(req.body);
+    // const validation = articleValidation(req.body);
+    // const articleSchema = ArticleModel(req.body);
     res.status(201).send({ message: "test" });
   },
   getArticle: async function (req, res) {
