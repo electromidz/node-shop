15804bbf5cdf8cb76d2e19bb922636e5e0847b1a
electromidz <electromidz@gmail.com>
2023-07-25 22:41:08 +0330

    Give me time please

--- controllers/article.js
+++ controllers/article.js
@@ -2,12 +2,12 @@ const { ArticleModel, articleValidation } = require("../models/article");
 module.exports = {
   postArticle: async function (req, res) {
     console.log("POST Article", req.body);
-    const validation = articleValidation(req.body);
-    const articleSchema = ArticleModel(req.body);
+    // const validation = articleValidation(req.body);
+    // const articleSchema = ArticleModel(req.body);
     res.status(201).send({ message: "test" });
   },
   getArticle: async function (req, res) {
     // console.log("GET Article");
-    res.status(201).send({ result: [] });
+    // res.status(201).send({ result: [] });
   },
 };
