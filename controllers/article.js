f358158d259caedcf09ec2cb1f82af272af74f57
electromidz <electromidz@gmail.com>
2023-07-26 23:05:11 +0330

    Work on article

--- controllers/article.js
+++ controllers/article.js
@@ -2,7 +2,7 @@ const { ArticleModel, articleValidation } = require("../models/article");
 module.exports = {
   postArticle: async function (req, res) {
     console.log("POST Article", req.body);
-    // const validation = articleValidation(req.body);
+    const validation = articleValidation(req.body);
     // const articleSchema = ArticleModel(req.body);
     res.status(201).send({ message: "test" });
   },
