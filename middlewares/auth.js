ed48233165cf3d71d6a89be3ccc995f9b7aac85f
electromidz <electromidz@gmail.com>
2023-07-30 23:04:48 +0330

    He wants to talk to you

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,14 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 function auth(req, res, next) {
-  // console.log(req.headers);
-  // const token = req.headers["authorization"];
-  // if (!token) {
-  //   return res.status(401).send({ message: "Not authenticated!" });
-  // } else {
-  //   jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
-  //     if (err) res.status(401).send({ message: err.message });
-  //     req.user = decoded;
+  console.log(req.headers);
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(401).send({ message: "Not authenticated!" });
+  } else {
+    jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, decoded) => {
+      if (err) res.status(401).send({ message: err.message });
+      req.user = decoded;
   next();
   //   });
   // }
