b90c771f131d1b3b42136e89f984a940092f7a1d
electromidz <electromidz@gmail.com>
2023-09-06 23:12:05 +0330

    Some things on it

--- routes/article.js
+++ routes/article.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/article");
 
-router.post("/", articleController.postArticle);
-router.get("/", articleController.geArticle);
-router.put("/", articleController.putArticle);
-router.delete("/", articleController.deleteArticle);
+// router.post("/", articleController.postArticle);
+// router.get("/", articleController.geArticle);
+// router.put("/", articleController.putArticle);
+// router.delete("/", articleController.deleteArticle);
 
 module.exports = router;
