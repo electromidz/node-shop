15844d6b992f0fb36ba45d42c10fdf0b6c9e37b1
electromidz <electromidz@gmail.com>
2023-09-10 23:41:12 +0330

    Routers

--- routes/article.js
+++ routes/article.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/article");
 
-router.post("/", articleController.postArticle);
+// router.post("/", articleController.postArticle);
 router.get("/", articleController.geArticle);
 router.put("/", articleController.putArticle);
 router.delete("/", articleController.deleteArticle);
