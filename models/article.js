2f511b20c1d1f97bf3d29af675087b92a0f607c5
electromidz <electromidz@gmail.com>
2023-09-11 23:33:30 +0330

    [CHANGE]

--- models/article.js
+++ models/article.js
@@ -2,35 +2,35 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const { z } = require("zod");
 
-const articleSchema = new Schema({
-  title: {
-    type: String,
-    minLength: 3,
-    maxLength: 100,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  product: {
-    type: Schema.Types.ObjectId,
-    ref:'product'
-  }
-  data: {
-    type: String,
-  },
-  created_at: { type: Date },
-  updated_at: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
-  { versionKey: false },
-  { strict: false }
-});
+// const articleSchema = new Schema({
+//   title: {
+//     type: String,
+//     minLength: 3,
+//     maxLength: 100,
+//     required: true,
+//   },
+//   content: {
+//     type: String,
+//     required: true,
+//   },
+//   owner: {
+//     type: Schema.Types.ObjectId,
+//     ref: "user",
+//     required: true,
+//   },
+//   product: {
+//     type: Schema.Types.ObjectId,
+//     ref:'product'
+//   }
+//   data: {
+//     type: String,
+//   },
+//   created_at: { type: Date },
+//   updated_at: { type: Date, default: Date.now },
+//   updated: { type: Date, default: Date.now },
+//   { versionKey: false },
+//   { strict: false }
+// });
 
 function articleValidation(article) {
   const schema = z
