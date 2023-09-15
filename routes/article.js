e98dd610658d4461a9a2090d63e5fc804b1029e4
electromidz <electromidz@gmail.com>
2023-09-15 13:43:49 +0330

    Change

--- routes/article.js
+++ routes/article.js
@@ -5,6 +5,6 @@ const articleController = require("../controllers/article");
 router.post("/", articleController.postArticle);
 router.get("/", articleController.geArticle);
 router.put("/", articleController.putArticle);
-// router.delete("/", articleController.deleteArticle);
+router.delete("/", articleController.deleteArticle);
 
 module.exports = router;
