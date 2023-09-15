4285dc91dabe0e71aa65628d241b4210e70c6a8c
electromidz <electromidz@gmail.com>
2023-09-15 13:44:07 +0330

    Change yourself

--- routes/article.js
+++ routes/article.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const articleController = require("../controllers/article");
 
 router.post("/", articleController.postArticle);
-router.get("/", articleController.geArticle);
-router.put("/", articleController.putArticle);
-router.delete("/", articleController.deleteArticle);
+// router.get("/", articleController.geArticle);
+// router.put("/", articleController.putArticle);
+// router.delete("/", articleController.deleteArticle);
 
 module.exports = router;
