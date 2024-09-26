b55330c3a62c88a6837255ba8f7f370c15e5a238
Omid Moghadas <49905980+electromidz@users.noreply.github.com>
2024-09-26 21:59:38 +0330

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
 
 mongoose.connection.on("error", (error) => {
   console.error(`Error connecting to DATABASE: ${error}`);
-});
+}); hi
 
 mongoose.connection.on("open", () => {
   console.log(
